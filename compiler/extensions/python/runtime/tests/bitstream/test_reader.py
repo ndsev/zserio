91b870fa4afeaf3dec113a9346c2e96c113cd990
Milan Kriz <milan.kriz@eccam.com>
2018-10-23 17:28:11 +0200

    [#40] Fix deprecated asserts

--- compiler/extensions/python/runtime/tests/bitstream/test_reader.py
+++ compiler/extensions/python/runtime/tests/bitstream/test_reader.py
@@ -9,12 +9,12 @@ class BitStreamReaderTest(unittest.TestCase):
         data = [0, 1, 255, 128, 127]
         reader = BitStreamReader(bytes(data))
         for byte in data:
-            self.assertEquals(byte, reader.readBits(8))
+            self.assertEqual(byte, reader.readBits(8))
 
         with self.assertRaises(PythonRuntimeException):
             reader.readBits(-1)
 
-        self.assertEquals(0, reader.readBits(0)) # read 0 bits
+        self.assertEqual(0, reader.readBits(0)) # read 0 bits
 
         with self.assertRaises(PythonRuntimeException):
             reader.readBits(1) # no more bits available
@@ -22,13 +22,13 @@ class BitStreamReaderTest(unittest.TestCase):
     def testReadSignedBits(self):
         data = [0, 0xff, 1, 127, 0x80]
         reader = BitStreamReader(bytes(data))
-        self.assertEquals(0, reader.readSignedBits(8))
-        self.assertEquals(-1, reader.readSignedBits(8)) # 0xff == -1
-        self.assertEquals(1, reader.readSignedBits(8))
-        self.assertEquals(127, reader.readSignedBits(8))
-        self.assertEquals(-128, reader.readSignedBits(8)) # 0x80 == -128
+        self.assertEqual(0, reader.readSignedBits(8))
+        self.assertEqual(-1, reader.readSignedBits(8)) # 0xff == -1
+        self.assertEqual(1, reader.readSignedBits(8))
+        self.assertEqual(127, reader.readSignedBits(8))
+        self.assertEqual(-128, reader.readSignedBits(8)) # 0x80 == -128
 
-        self.assertEquals(0, reader.readSignedBits(0)) # read 0 bits
+        self.assertEqual(0, reader.readSignedBits(0)) # read 0 bits
 
         with self.assertRaises(PythonRuntimeException):
             reader.readSignedBits(1) # no more bits available
