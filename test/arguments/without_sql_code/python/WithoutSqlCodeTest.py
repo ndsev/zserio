1c85f17c0153d1973edd0786eefba49e1a96e8c3
Milan Kriz <milan.kriz@eccam.com>
2019-01-29 14:32:28 +0100

    [#88] Add without_sql_code argument test

---
+++ test/arguments/without_sql_code/python/WithoutSqlCodeTest.py
@@ -0,0 +1,28 @@
+import os
+import pathlib
+
+import unittest
+
+from testutils import getZserioApi
+
+class WithoutSqlCodeTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.api = getZserioApi(__file__, "without_sql_code.zs", extraArgs=["-withoutSqlCode"])
+
+    def testCheckGeoMapTable(self):
+        self.assertFalse(hasattr(self.api, "GeoMapTable"))
+        self.assertFalse(self._isFilePresent("GeoMapTable.py"))
+
+    def testCheckWorldDb(self):
+        self.assertFalse(hasattr(self.api, "WorldDb"))
+        self.assertFalse(self._isFilePresent("WorldDb.py"))
+
+    def testCheckTile(self):
+        self.assertTrue(hasattr(self.api, "Tile"))
+        self.assertTrue(self._isFilePresent("Tile.py"))
+
+    def _isFilePresent(self, filename):
+        fileFullPath = os.path.join(os.path.abspath(os.path.join(self.api.__file__, "..")), filename)
+        file = pathlib.Path(fileFullPath)
+        return file.is_file()
