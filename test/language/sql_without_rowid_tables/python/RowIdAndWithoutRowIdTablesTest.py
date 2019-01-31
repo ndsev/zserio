4a3feae79594c7bc4f451cc6b22c114c4e34baa1
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2019-01-31 12:28:08 +0100

    [#88] Implement language sql_without_rowid_tables tests

---
+++ test/language/sql_without_rowid_tables/python/RowIdAndWithoutRowIdTablesTest.py
@@ -0,0 +1,48 @@
+import unittest
+import os
+import apsw
+
+from testutils import getZserioApi, getApiDir
+
+class SimpleWithoutRowIdTableTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.api = getZserioApi(__file__, "sql_without_rowid_tables.zs").rowid_and_without_rowid_tables
+        cls._fileName = os.path.join(getApiDir(os.path.dirname(__file__)),
+                                     "rowid_and_without_rowid_tables_test.sqlite")
+
+    def setUp(self):
+        if os.path.exists(self._fileName):
+            os.remove(self._fileName)
+        self._database = self.api.RowIdAndWithoutRowIdDb.fromFile(self._fileName)
+
+    def tearDown(self):
+        self._database.close()
+
+    def testRowIdColumn(self):
+        self._database.createSchema()
+        self.assertFalse(self._isColumnInTable("rowid", self.WITHOUT_ROWID_TABLE_NAME))
+        self.assertTrue(self._isColumnInTable("rowid", self.ORDINARY_ROWID_TABLE_NAME))
+
+    def testCreateOrdinaryRowIdTable(self):
+        testTable = self._database.getWithoutRowIdTable()
+        testTable.createOrdinaryRowIdTable()
+        self.assertTrue(self._isColumnInTable("rowid", self.WITHOUT_ROWID_TABLE_NAME))
+
+    def testWithoutRowIdTableNamesBlackList(self):
+        self._database.createSchema([self.WITHOUT_ROWID_TABLE_NAME])
+        self.assertTrue(self._isColumnInTable("rowid", self.WITHOUT_ROWID_TABLE_NAME))
+        self.assertTrue(self._isColumnInTable("rowid", self.ORDINARY_ROWID_TABLE_NAME))
+
+    def _isColumnInTable(self, columnName, tableName):
+        sqlQuery = "SELECT " + columnName + " FROM " + tableName + " LIMIT 0"
+
+        # try select to check if hidden column exists
+        try:
+            self._database.executeQuery(sqlQuery)
+            return True
+        except apsw.SQLError:
+            return False
+
+    WITHOUT_ROWID_TABLE_NAME = "withoutRowIdTable"
+    ORDINARY_ROWID_TABLE_NAME = "ordinaryRowIdTable"
---
+++ test/language/sql_without_rowid_tables/python/SimpleWithoutRowIdTablesTest.py
@@ -0,0 +1,46 @@
+import unittest
+import os
+import apsw
+
+from testutils import getZserioApi, getApiDir
+
+class SimpleWithoutRowIdTableTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.api = getZserioApi(__file__, "sql_without_rowid_tables.zs").simple_without_rowid_table
+        cls._fileName = os.path.join(getApiDir(os.path.dirname(__file__)),
+                                     "simple_without_rowid_table_test.sqlite")
+
+    def setUp(self):
+        if os.path.exists(self._fileName):
+            os.remove(self._fileName)
+        self._database = self.api.SimpleWithoutRowIdDb.fromFile(self._fileName)
+
+    def tearDown(self):
+        self._database.close()
+
+    def testRowIdColumn(self):
+        self._database.createSchema()
+        self.assertFalse(self._isColumnInTable("rowid", self.TABLE_NAME))
+
+    def testCreateOrdinaryRowIdTable(self):
+        testTable = self._database.getSimpleWithoutRowIdTable()
+        testTable.createOrdinaryRowIdTable()
+        self.assertTrue(self._isColumnInTable("rowid", self.TABLE_NAME))
+
+    def testCheckWithoutRowIdTableNamesBlackList(self):
+        withoutRowIdTableNamesBlackList = [self.TABLE_NAME]
+        self._database.createSchema(withoutRowIdTableNamesBlackList)
+        self.assertTrue(self._isColumnInTable("rowid", self.TABLE_NAME))
+
+    def _isColumnInTable(self, columnName, tableName):
+        sqlQuery = "SELECT " + columnName + " FROM " + tableName + " LIMIT 0"
+
+        # try select to check if hidden column exists
+        try:
+            self._database.executeQuery(sqlQuery)
+            return True
+        except apsw.SQLError:
+            return False
+
+    TABLE_NAME = "simpleWithoutRowIdTable"
