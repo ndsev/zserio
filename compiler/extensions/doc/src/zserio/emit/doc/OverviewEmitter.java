6965169eb997e7a11e22da5e89124990a8a83f53
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2019-05-14 08:06:36 +0200

    [#123] Fix Doc emitter not to use getLocalTypes

--- compiler/extensions/doc/src/zserio/emit/doc/OverviewEmitter.java
+++ compiler/extensions/doc/src/zserio/emit/doc/OverviewEmitter.java
@@ -10,8 +10,17 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
+import zserio.ast.ChoiceType;
+import zserio.ast.ConstType;
+import zserio.ast.EnumType;
 import zserio.ast.Package;
 import zserio.ast.Root;
+import zserio.ast.ServiceType;
+import zserio.ast.SqlDatabaseType;
+import zserio.ast.SqlTableType;
+import zserio.ast.StructureType;
+import zserio.ast.Subtype;
+import zserio.ast.UnionType;
 import zserio.ast.ZserioType;
 import zserio.emit.common.ZserioEmitException;
 import zserio.tools.StringJoinUtil;
@@ -35,14 +44,14 @@ public class OverviewEmitter extends DefaultHtmlEmitter
     {
         try
         {
-            for (Package pkg : packages)
+            for (Map.Entry<Package, List<ZserioType>> packageEntry : packages.entrySet())
             {
-                for (ZserioType type : pkg.getLocalTypes())
+                for (ZserioType type : packageEntry.getValue())
                 {
                     final String typeName = type.getName();
                     boolean isDoubleDefinedType = Boolean.TRUE.equals(doubleTypeNames.get(typeName));
                     LinkedType linkedType = new LinkedType(type, isDoubleDefinedType);
-                    typeMap.put(getFullTypeName(typeName, pkg), linkedType);
+                    typeMap.put(getFullTypeName(typeName, packageEntry.getKey()), linkedType);
                 }
             }
 
@@ -70,7 +79,13 @@ public class OverviewEmitter extends DefaultHtmlEmitter
     public void beginPackage(Package packageToken) throws ZserioEmitException
     {
         super.beginPackage(packageToken);
-        for (ZserioType type : currentPackage.getLocalTypes())
+        localTypes = new ArrayList<ZserioType>();
+    }
+
+    @Override
+    public void endPackage(Package packageToken) throws ZserioEmitException
+    {
+        for (ZserioType type : localTypes)
         {
             final String typeName = type.getName();
             if (doubleTypeNames.containsKey(typeName))
@@ -81,7 +96,61 @@ public class OverviewEmitter extends DefaultHtmlEmitter
         String pkgName = currentPackage.getPackageName().toString();
         pkgName = pkgName.replace('.', '_');
         packageNames.add(pkgName);
-        packages.add(currentPackage);
+        packages.put(currentPackage, localTypes);
+    }
+
+    @Override
+    public void beginConst(ConstType constType) throws ZserioEmitException
+    {
+        localTypes.add(constType);
+    }
+
+    @Override
+    public void beginSubtype(Subtype subType) throws ZserioEmitException
+    {
+        localTypes.add(subType);
+    }
+
+    @Override
+    public void beginStructure(StructureType structureType) throws ZserioEmitException
+    {
+        localTypes.add(structureType);
+    }
+
+    @Override
+    public void beginChoice(ChoiceType choiceType) throws ZserioEmitException
+    {
+        localTypes.add(choiceType);
+    }
+
+    @Override
+    public void beginUnion(UnionType unionType) throws ZserioEmitException
+    {
+        localTypes.add(unionType);
+    }
+
+    @Override
+    public void beginEnumeration(EnumType enumType) throws ZserioEmitException
+    {
+        localTypes.add(enumType);
+    }
+
+    @Override
+    public void beginSqlTable(SqlTableType sqlTableType) throws ZserioEmitException
+    {
+        localTypes.add(sqlTableType);
+    }
+
+    @Override
+    public void beginSqlDatabase(SqlDatabaseType sqlDatabaseType) throws ZserioEmitException
+    {
+        localTypes.add(sqlDatabaseType);
+    }
+
+    @Override
+    public void beginService(ServiceType service) throws ZserioEmitException
+    {
+        localTypes.add(service);
     }
 
     public Collection<LinkedType> getTypes()
@@ -112,5 +181,6 @@ public class OverviewEmitter extends DefaultHtmlEmitter
     private final Map<String, LinkedType> typeMap = new TreeMap<String, LinkedType>();
     private final Map<String, Boolean> doubleTypeNames = new HashMap<String, Boolean>();
     private final HashSet<String> packageNames = new HashSet<String>();
-    private final Set<Package> packages = new HashSet<Package>();
+    private final Map<Package, List<ZserioType>> packages = new HashMap<Package, List<ZserioType>>();
+    private List<ZserioType> localTypes = new ArrayList<ZserioType>();
 }
