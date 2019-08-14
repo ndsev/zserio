285ae1fb66afe113f9b9157e2408e72a54bc714d
Milan Kriz <milan.kriz@eccam.com>
2019-08-14 15:49:36 +0200

    [#108] Add offets language tests

---
+++ test/language/offsets/cpp/NestedOffsetTest.cpp
@@ -0,0 +1,224 @@
+#include "gtest/gtest.h"
+
+#include "zserio/BitStreamWriter.h"
+#include "zserio/BitStreamReader.h"
+
+#include "offsets/nested_offset/NestedOffset.h"
+
+namespace offsets
+{
+namespace nested_offset
+{
+
+class NestedOffsetTest : public ::testing::Test
+{
+protected:
+    void writeNestedOffsetToByteArray(zserio::BitStreamWriter& writer, bool writeWrongOffsets)
+    {
+        const uint32_t wrongTerminatorOffset = WRONG_TERMINATOR_OFFSET;
+        const uint32_t correctTerminatorOffset = TERMINATOR_OFFSET;
+        writer.writeBits((writeWrongOffsets) ? wrongTerminatorOffset : correctTerminatorOffset, 32);
+        writer.writeBool(BOOL_VALUE);
+        writer.writeVarUInt64(NestedOffsetUnion::CHOICE_nestedOffsetArrayStructure); // union's choice tag
+        writer.writeBits(NUM_ELEMENTS, 8);
+        for (short i = 0; i < NUM_ELEMENTS; ++i)
+        {
+            writer.writeBits((writeWrongOffsets) ? WRONG_DATA_OFFSET : FIRST_DATA_OFFSET + i * 8, 32);
+            writer.writeBits(0, (i == 0) ? 7 : 1);
+            writer.writeBits(i, 31);
+        }
+
+        writer.writeBits(TERMINATOR_VALUE, 7);
+    }
+
+    void checkNestedOffset(const NestedOffset& nestedOffset)
+    {
+        const uint32_t expectedTerminatorOffset = TERMINATOR_OFFSET;
+        ASSERT_EQ(expectedTerminatorOffset, nestedOffset.getTerminatorOffset());
+        const bool expectedBoolValue = BOOL_VALUE;
+        ASSERT_EQ(expectedBoolValue, nestedOffset.getBoolValue());
+
+        const NestedOffsetChoice& nestedOffsetChoice = nestedOffset.getNestedOffsetChoice();
+        ASSERT_EQ(expectedBoolValue, nestedOffsetChoice.getType());
+
+        const NestedOffsetUnion& nestedOffsetUnion = nestedOffsetChoice.getNestedOffsetUnion();
+        ASSERT_EQ(NestedOffsetUnion::CHOICE_nestedOffsetArrayStructure, nestedOffsetUnion.choiceTag());
+
+        const NestedOffsetArrayStructure& nestedOffsetArrayStructure =
+                nestedOffsetUnion.getNestedOffsetArrayStructure();
+        const uint8_t expectedNumElements = NUM_ELEMENTS;
+        ASSERT_EQ(expectedNumElements, nestedOffsetArrayStructure.getNumElements());
+
+        const std::vector<NestedOffsetStructure>& nestedOffsetStructureList =
+                nestedOffsetArrayStructure.getNestedOffsetStructureList();
+        ASSERT_EQ(expectedNumElements, nestedOffsetStructureList.size());
+        for (short i = 0; i < NUM_ELEMENTS; ++i)
+        {
+            const NestedOffsetStructure& nestedOffsetStructure = nestedOffsetStructureList[i];
+            ASSERT_EQ(FIRST_DATA_OFFSET + i * 8L, nestedOffsetStructure.getDataOffset());
+            ASSERT_EQ(i, nestedOffsetStructure.getData());
+        }
+    }
+
+    void fillNestedOffset(NestedOffset& nestedOffset, bool createWrongOffsets)
+    {
+        const uint32_t wrongTerminatorOffset = WRONG_TERMINATOR_OFFSET;
+        const uint32_t correctTerminatorOffset = TERMINATOR_OFFSET;
+        const uint32_t terminatorOffset = (createWrongOffsets)
+                ? wrongTerminatorOffset : correctTerminatorOffset;
+        nestedOffset.setTerminatorOffset(terminatorOffset);
+        nestedOffset.setBoolValue(BOOL_VALUE);
+        NestedOffsetChoice& nestedOffsetChoice = nestedOffset.getNestedOffsetChoice();
+        fillNestedOffsetChoice(nestedOffsetChoice, createWrongOffsets);
+        nestedOffset.setTerminator(TERMINATOR_VALUE);
+        nestedOffset.initializeChildren();
+    }
+
+    void fillNestedOffsetChoice(NestedOffsetChoice& nestedOffsetChoice, bool createWrongOffsets)
+    {
+        NestedOffsetUnion nestedOffsetUnion;
+        NestedOffsetArrayStructure nestedOffsetArrayStructure;
+        nestedOffsetArrayStructure.setNumElements(NUM_ELEMENTS);
+        std::vector<NestedOffsetStructure>& nestedOffsetStructureList =
+                nestedOffsetArrayStructure.getNestedOffsetStructureList();
+        nestedOffsetStructureList.reserve(NUM_ELEMENTS);
+        for (short i = 0; i < NUM_ELEMENTS; ++i)
+        {
+            const uint32_t dataOffset = (createWrongOffsets) ? WRONG_DATA_OFFSET : FIRST_DATA_OFFSET + i * 8;
+            NestedOffsetStructure nestedOffsetStructure;
+            nestedOffsetStructure.setDataOffset(dataOffset);
+            nestedOffsetStructure.setData(i);
+            nestedOffsetStructureList.push_back(nestedOffsetStructure);
+        }
+        nestedOffsetUnion.setNestedOffsetArrayStructure(nestedOffsetArrayStructure);
+        nestedOffsetChoice.setNestedOffsetUnion(nestedOffsetUnion);
+    }
+
+    static const bool     BOOL_VALUE = true;
+    static const uint8_t  NUM_ELEMENTS = 2;
+
+    static const uint32_t WRONG_TERMINATOR_OFFSET = 0;
+    static const uint32_t TERMINATOR_OFFSET = 7 + NUM_ELEMENTS * 8;
+
+    static const uint32_t WRONG_DATA_OFFSET = 0;
+    static const uint32_t FIRST_DATA_OFFSET = 7 + 4;
+
+    static const uint8_t  TERMINATOR_VALUE = 0x45;
+
+    static const size_t   NEST_OFFSET_BIT_SIZE = TERMINATOR_OFFSET * 8 + 7;
+};
+
+TEST_F(NestedOffsetTest, read)
+{
+    const bool writeWrongOffsets = false;
+    zserio::BitStreamWriter writer;
+    writeNestedOffsetToByteArray(writer, writeWrongOffsets);
+    size_t writeBufferByteSize;
+    const uint8_t* writeBuffer = writer.getWriteBuffer(writeBufferByteSize);
+    zserio::BitStreamReader reader(writeBuffer, writeBufferByteSize);
+
+    NestedOffset nestedOffset(reader);
+    checkNestedOffset(nestedOffset);
+}
+
+TEST_F(NestedOffsetTest, readWrongOffsets)
+{
+    const bool writeWrongOffsets = true;
+    zserio::BitStreamWriter writer;
+    writeNestedOffsetToByteArray(writer, writeWrongOffsets);
+    size_t writeBufferByteSize;
+    const uint8_t* writeBuffer = writer.getWriteBuffer(writeBufferByteSize);
+    zserio::BitStreamReader reader(writeBuffer, writeBufferByteSize);
+
+    EXPECT_THROW(NestedOffset nestedOffset(reader), zserio::CppRuntimeException);
+}
+
+TEST_F(NestedOffsetTest, bitSizeOf)
+{
+    const bool createWrongOffsets = false;
+    NestedOffset nestedOffset;
+    fillNestedOffset(nestedOffset, createWrongOffsets);
+
+    const size_t expectedBitSize = NEST_OFFSET_BIT_SIZE;
+    ASSERT_EQ(expectedBitSize, nestedOffset.bitSizeOf());
+}
+
+TEST_F(NestedOffsetTest, bitSizeOfWithPosition)
+{
+    const bool createWrongOffsets = false;
+    NestedOffset nestedOffset;
+    fillNestedOffset(nestedOffset, createWrongOffsets);
+
+    const size_t bitPosition = 2;
+    const size_t expectedBitSize = NEST_OFFSET_BIT_SIZE - bitPosition;
+    ASSERT_EQ(expectedBitSize, nestedOffset.bitSizeOf(bitPosition));
+}
+
+TEST_F(NestedOffsetTest, initializeOffsets)
+{
+    const bool createWrongOffsets = true;
+    NestedOffset nestedOffset;
+    fillNestedOffset(nestedOffset, createWrongOffsets);
+
+    const size_t bitPosition = 0;
+    const size_t expectedBitSize = NEST_OFFSET_BIT_SIZE;
+    ASSERT_EQ(expectedBitSize, nestedOffset.initializeOffsets(bitPosition));
+    checkNestedOffset(nestedOffset);
+}
+
+TEST_F(NestedOffsetTest, initializeOffsetsWithPosition)
+{
+    const bool createWrongOffsets = true;
+    NestedOffset nestedOffset;
+    fillNestedOffset(nestedOffset, createWrongOffsets);
+
+    const size_t bitPosition = 2;
+    const size_t expectedBitSize = NEST_OFFSET_BIT_SIZE;
+    ASSERT_EQ(expectedBitSize, nestedOffset.initializeOffsets(bitPosition));
+    checkNestedOffset(nestedOffset);
+}
+
+TEST_F(NestedOffsetTest, write)
+{
+    const bool createWrongOffsets = true;
+    NestedOffset nestedOffset;
+    fillNestedOffset(nestedOffset, createWrongOffsets);
+
+    zserio::BitStreamWriter writer;
+    nestedOffset.write(writer);
+    checkNestedOffset(nestedOffset);
+
+    size_t writeBufferByteSize;
+    const uint8_t* writeBuffer = writer.getWriteBuffer(writeBufferByteSize);
+    zserio::BitStreamReader reader(writeBuffer, writeBufferByteSize);
+    NestedOffset readNestedOffset(reader);
+    checkNestedOffset(readNestedOffset);
+    ASSERT_TRUE(nestedOffset == readNestedOffset);
+}
+
+TEST_F(NestedOffsetTest, writeWithPosition)
+{
+    const bool createWrongOffsets = true;
+    NestedOffset nestedOffset;
+    fillNestedOffset(nestedOffset, createWrongOffsets);
+
+    zserio::BitStreamWriter writer;
+    const size_t bitPosition = 2;
+    writer.writeBits(0, bitPosition);
+    nestedOffset.write(writer);
+
+    checkNestedOffset(nestedOffset);
+}
+
+TEST_F(NestedOffsetTest, writeWrongOffsets)
+{
+    const bool createWrongOffsets = true;
+    NestedOffset nestedOffset;
+    fillNestedOffset(nestedOffset, createWrongOffsets);
+
+    zserio::BitStreamWriter writer;
+    ASSERT_THROW(nestedOffset.write(writer, zserio::NO_PRE_WRITE_ACTION), zserio::CppRuntimeException);
+}
+
+} // namespace nested_offset
+} // namespace offsets
---
+++ test/language/offsets/cpp/OptionalMemberOffsetTest.cpp
@@ -0,0 +1,189 @@
+#include "gtest/gtest.h"
+
+#include "zserio/BitStreamWriter.h"
+#include "zserio/BitStreamReader.h"
+
+#include "offsets/optional_member_offset/OptionalMemberOffset.h"
+
+namespace offsets
+{
+namespace optional_member_offset
+{
+
+class OptionalMemberOffsetTest : public ::testing::Test
+{
+protected:
+    void writeOptionalMemberOffsetToByteArray(zserio::BitStreamWriter& writer, bool hasOptional,
+            uint32_t optionalFieldOffset, int32_t optionalField, int32_t field)
+    {
+        writer.writeBool(hasOptional);
+        writer.writeBits(optionalFieldOffset, 32);
+
+        if (hasOptional)
+        {
+            writer.writeBits(0, 7);
+            writer.writeSignedBits(optionalField, 32);
+        }
+
+        writer.writeSignedBits(field, 32);
+    }
+
+    void checkOptionalMemberOffset(const OptionalMemberOffset& optionalMemberOffset, bool hasOptional,
+            uint32_t optionalFieldOffset, int32_t optionalField, int32_t field)
+    {
+        ASSERT_EQ(hasOptional, optionalMemberOffset.getHasOptional());
+        ASSERT_EQ(optionalFieldOffset, optionalMemberOffset.getOptionalFieldOffset());
+
+        if (hasOptional)
+        {
+            ASSERT_EQ(optionalField, *optionalMemberOffset.getOptionalField());
+            ASSERT_TRUE(optionalMemberOffset.hasOptionalField());
+        }
+        else
+        {
+            ASSERT_FALSE(optionalMemberOffset.hasOptionalField());
+        }
+
+        ASSERT_EQ(field, (int)optionalMemberOffset.getField());
+    }
+
+    void fillOptionalMemberOffset(OptionalMemberOffset& optionalMemberOffset, bool hasOptional,
+            uint32_t optionalFieldOffset, int32_t optionalField, int32_t field)
+    {
+        optionalMemberOffset.setHasOptional(hasOptional);
+        optionalMemberOffset.setOptionalFieldOffset(optionalFieldOffset);
+        if (hasOptional)
+            optionalMemberOffset.setOptionalField(optionalField);
+        optionalMemberOffset.setField(field);
+    }
+
+    static const size_t WITH_OPTIONAL_MEMBER_OFFSET_BIT_SIZE = 104;
+    static const size_t WITHOUT_OPTIONAL_MEMBER_OFFSET_BIT_SIZE = 65;
+
+    static const size_t OPTIONAL_FIELD_OFFSET = 5;
+    static const size_t WRONG_OPTIONAL_FIELD_OFFSET = 0;
+};
+
+TEST_F(OptionalMemberOffsetTest, readWithOptional)
+{
+    const bool hasOptional = true;
+    const int32_t optionalField = 0x1212;
+    const int32_t field = 0x2121;
+    zserio::BitStreamWriter writer;
+    writeOptionalMemberOffsetToByteArray(writer, hasOptional, OPTIONAL_FIELD_OFFSET, optionalField, field);
+    size_t writeBufferByteSize;
+    const uint8_t* writeBuffer = writer.getWriteBuffer(writeBufferByteSize);
+    zserio::BitStreamReader reader(writeBuffer, writeBufferByteSize);
+
+    OptionalMemberOffset optionalMemberOffset(reader);
+    checkOptionalMemberOffset(optionalMemberOffset, hasOptional, OPTIONAL_FIELD_OFFSET, optionalField, field);
+}
+
+TEST_F(OptionalMemberOffsetTest, readWithoutOptional)
+{
+    const bool hasOptional = false;
+    const uint32_t optionalFieldOffset = 0xABCD;
+    const int32_t optionalField = 0;
+    const int32_t field = 0x2121;
+    zserio::BitStreamWriter writer;
+    writeOptionalMemberOffsetToByteArray(writer, hasOptional, optionalFieldOffset, optionalField, field);
+    size_t writeBufferByteSize;
+    const uint8_t* writeBuffer = writer.getWriteBuffer(writeBufferByteSize);
+    zserio::BitStreamReader reader(writeBuffer, writeBufferByteSize);
+
+    OptionalMemberOffset optionalMemberOffset(reader);
+    checkOptionalMemberOffset(optionalMemberOffset, hasOptional, optionalFieldOffset, optionalField, field);
+}
+
+TEST_F(OptionalMemberOffsetTest, bitSizeOfWithOptional)
+{
+    OptionalMemberOffset optionalMemberOffset;
+    fillOptionalMemberOffset(optionalMemberOffset, true, OPTIONAL_FIELD_OFFSET, 0x1010, 0x2020);
+
+    const size_t expectedBitSize = WITH_OPTIONAL_MEMBER_OFFSET_BIT_SIZE;
+    ASSERT_EQ(expectedBitSize, optionalMemberOffset.bitSizeOf());
+}
+
+TEST_F(OptionalMemberOffsetTest, bitSizeOfWithoutOptional)
+{
+    OptionalMemberOffset optionalMemberOffset;
+    fillOptionalMemberOffset(optionalMemberOffset, false, 0xDEAD, 0, 0xBEEF);
+
+    const size_t expectedBitSize = WITHOUT_OPTIONAL_MEMBER_OFFSET_BIT_SIZE;
+    ASSERT_EQ(expectedBitSize, optionalMemberOffset.bitSizeOf());
+}
+
+TEST_F(OptionalMemberOffsetTest, initializeOffsetsWithOptional)
+{
+    const bool hasOptional = true;
+    const uint32_t optionalField = 0x1010;
+    const int32_t field = 0x2020;
+    OptionalMemberOffset optionalMemberOffset;
+    fillOptionalMemberOffset(optionalMemberOffset, hasOptional, WRONG_OPTIONAL_FIELD_OFFSET, optionalField,
+            field);
+
+    const size_t bitPosition = 2;
+    const size_t expectedBitSize = WITH_OPTIONAL_MEMBER_OFFSET_BIT_SIZE;
+    ASSERT_EQ(expectedBitSize, optionalMemberOffset.initializeOffsets(bitPosition));
+    checkOptionalMemberOffset(optionalMemberOffset, hasOptional, OPTIONAL_FIELD_OFFSET, optionalField, field);
+}
+
+TEST_F(OptionalMemberOffsetTest, initializeOffsetsWithoutOptional)
+{
+    const bool hasOptional = false;
+    const uint32_t optionalFieldOffset = 0xABCD;
+    const int32_t optionalField = 0;
+    const int32_t field = 0x2020;
+    OptionalMemberOffset optionalMemberOffset;
+    fillOptionalMemberOffset(optionalMemberOffset, hasOptional, optionalFieldOffset, optionalField, field);
+
+    const size_t bitPosition = 2;
+    const size_t expectedBitSize = WITHOUT_OPTIONAL_MEMBER_OFFSET_BIT_SIZE;
+    ASSERT_EQ(expectedBitSize + bitPosition, optionalMemberOffset.initializeOffsets(bitPosition));
+    checkOptionalMemberOffset(optionalMemberOffset, hasOptional, optionalFieldOffset, optionalField, field);
+}
+
+TEST_F(OptionalMemberOffsetTest, writeWithOptional)
+{
+    const bool hasOptional = true;
+    const int32_t optionalField = 0x1A1A;
+    const int32_t field = 0xA1A1;
+    OptionalMemberOffset optionalMemberOffset;
+    fillOptionalMemberOffset(optionalMemberOffset, hasOptional, WRONG_OPTIONAL_FIELD_OFFSET, optionalField,
+            field);
+
+    zserio::BitStreamWriter writer;
+    optionalMemberOffset.write(writer);
+    checkOptionalMemberOffset(optionalMemberOffset, hasOptional, OPTIONAL_FIELD_OFFSET, optionalField, field);
+
+    size_t writeBufferByteSize;
+    const uint8_t* writeBuffer = writer.getWriteBuffer(writeBufferByteSize);
+    zserio::BitStreamReader reader(writeBuffer, writeBufferByteSize);
+    OptionalMemberOffset readOptionalMemberOffset(reader);
+    checkOptionalMemberOffset(readOptionalMemberOffset, hasOptional, OPTIONAL_FIELD_OFFSET, optionalField,
+            field);
+    ASSERT_TRUE(optionalMemberOffset == readOptionalMemberOffset);
+}
+
+TEST_F(OptionalMemberOffsetTest, writeWithoutOptional)
+{
+    const bool hasOptional = false;
+    const uint32_t optionalFieldOffset = 0xABCE;
+    const int32_t optionalField = 0;
+    const int32_t field = 0x7ACF;
+    OptionalMemberOffset optionalMemberOffset;
+    fillOptionalMemberOffset(optionalMemberOffset, hasOptional, optionalFieldOffset, optionalField, field);
+
+    zserio::BitStreamWriter writer;
+    optionalMemberOffset.write(writer);
+
+    size_t writeBufferByteSize;
+    const uint8_t* writeBuffer = writer.getWriteBuffer(writeBufferByteSize);
+    zserio::BitStreamReader reader(writeBuffer, writeBufferByteSize);
+    OptionalMemberOffset readOptionalMemberOffset(reader);
+    checkOptionalMemberOffset(readOptionalMemberOffset, hasOptional, optionalFieldOffset, optionalField, field);
+    ASSERT_TRUE(optionalMemberOffset == readOptionalMemberOffset);
+}
+
+} // namespace optional_member_offset
+} // namespace offsets
