7fbf5b1dfa08a500684c70053ce6350c4b9506fb
Milan Kriz <milan.kriz@eccam.com>
2019-12-20 14:57:53 +0100

    [#129] Allow bitmask constants

--- compiler/core/src/zserio/ast/Constant.java
+++ compiler/core/src/zserio/ast/Constant.java
@@ -115,8 +115,13 @@ public class Constant extends DocumentableAstNode implements Comparable<Constant
     {
         // check base type
         final ZserioType baseType = typeInstantiation.getBaseType();
-        if (!(baseType instanceof BuiltInType) && !(baseType instanceof EnumType))
-            throw new ParserException(this, "Constants can be defined only for built-in types and enums!");
+        if (!(baseType instanceof BuiltInType) &&
+                !(baseType instanceof EnumType) &&
+                !(baseType instanceof BitmaskType))
+        {
+            throw new ParserException(this,
+                    "Constants can be defined only for built-in types, enums or bitmasks!");
+        }
 
         // check expression type
         ExpressionUtil.checkExpressionType(valueExpression, typeInstantiation);
