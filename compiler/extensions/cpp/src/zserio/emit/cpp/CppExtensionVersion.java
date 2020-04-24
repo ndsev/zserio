d42f1e2e4b357b86e3fa79885e46ebc7f30a3e03
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2020-04-24 10:57:23 +0200

    Change version to 2.0.0-pre4

--- compiler/extensions/cpp/src/zserio/emit/cpp/CppExtensionVersion.java
+++ compiler/extensions/cpp/src/zserio/emit/cpp/CppExtensionVersion.java
@@ -8,5 +8,5 @@ package zserio.emit.cpp;
 public class CppExtensionVersion
 {
     /** C++ extension version string. */
-    public static final String VERSION_STRING = "2.0.0-pre3";
+    public static final String VERSION_STRING = "2.0.0-pre4";
 }
