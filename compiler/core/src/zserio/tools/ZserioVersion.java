7d9a75443b9e271598972608745db0eebe405533
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2020-09-28 07:19:20 +0200

    Change version to 2.1.0-pre2

--- compiler/core/src/zserio/tools/ZserioVersion.java
+++ compiler/core/src/zserio/tools/ZserioVersion.java
@@ -6,5 +6,5 @@ package zserio.tools;
 public final class ZserioVersion
 {
     /** ZserioTool version string. */
-    public static final String VERSION_STRING = "2.1.0-pre1";
+    public static final String VERSION_STRING = "2.1.0-pre2";
 }
--- compiler/extensions/python/runtime/src/zserio/__init__.py
+++ compiler/extensions/python/runtime/src/zserio/__init__.py
@@ -22,4 +22,4 @@ import zserio.float
 import zserio.hashcode
 import zserio.limits
 
-VERSION_STRING = "2.1.0-pre1"
+VERSION_STRING = "2.1.0-pre2"
