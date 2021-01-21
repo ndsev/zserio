ad24d64c8e866327f70ee12d1ca6e8c1308c5f97
Milan Kriz <milan.kriz@eccam.com>
2021-01-21 18:13:54 +0100

    [#233] Try to fix pylint warnings on test formatting

--- test/arguments/with_python_properties/python/WithPythonPropertiesTest.py
+++ test/arguments/with_python_properties/python/WithPythonPropertiesTest.py
@@ -2,13 +2,13 @@ import unittest
 
 from testutils import getZserioApi
 
-class WithoutWriterCodeTest(unittest.TestCase):
+class WithPythonPropertiesTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.api = getZserioApi(__file__, "with_python_properties.zs", extraArgs=["-withPythonProperties"])
 
     def testVersionAvailabilityMethods(self):
-        userType =self.api.VersionAvailability
+        userType = self.api.VersionAvailability
 
         self._assertMethodNotPresent(userType, "getValue")
         self._assertPropertyPresent(userType, "value", readOnly=True)
@@ -23,6 +23,8 @@ class WithoutWriterCodeTest(unittest.TestCase):
 
         self._assertPropertyPresent(userType, "value16", readOnly=False)
         self._assertPropertyPresent(userType, "value32", readOnly=False)
+
+        self._assertPropertyPresent(userType, "choiceTag", readOnly=True)
 
     def testItemMethods(self):
         userType = self.api.Item
@@ -115,7 +117,7 @@ class WithoutWriterCodeTest(unittest.TestCase):
             tile.versionAvailability = self.api.VersionAvailability.Values.VERSION_NUMBER
             tile.version = i
             tile.numElements = NUM_ELEMENTS
-            tile.offsets = [ 0 ] * NUM_ELEMENTS
+            tile.offsets = [0] * NUM_ELEMENTS
             data = []
             for j in range(NUM_ELEMENTS):
                 itemChoiceHolder = self.api.ItemChoiceHolder()
@@ -146,34 +148,44 @@ class WithoutWriterCodeTest(unittest.TestCase):
         tileCol = 1
 
         readWorldDb = self.api.WorldDb(worldDb.connection)
-        readEuropeTiles = [ (row[tileIdCol], row[tileCol]) for row in readWorldDb.europe.read() ]
+        readEuropeTiles = [(row[tileIdCol], row[tileCol]) for row in readWorldDb.europe.read()]
         self.assertEqual(europeTiles, readEuropeTiles)
-        readAmericaTiles = [ (row[tileIdCol], row[tileCol]) for row in readWorldDb.america.read() ]
+        readAmericaTiles = [(row[tileIdCol], row[tileCol]) for row in readWorldDb.america.read()]
         self.assertEqual(americaTiles, readAmericaTiles)
         self.assertNotEqual(readEuropeTiles, readAmericaTiles)
 
     def _assertMethodNotPresent(self, userType, method):
         self.assertFalse(hasattr(userType, method),
-            msg=("Method '%s' is present in '%s'!" % (method, userType.__name__)))
+                         msg=("Method '%s' is present in '%s'!" % (method, userType.__name__)))
 
     def _assertMethodPresent(self, userType, method):
         self.assertTrue(hasattr(userType, method),
-            msg=("Method '%s' is not present in '%s'!" % (method, userType.__name__)))
+                        msg=("Method '%s' is not present in '%s'!" % (method, userType.__name__)))
 
     def _assertPropertyPresent(self, userType, prop, *, readOnly):
-        self.assertTrue(hasattr(userType, prop),
-            msg=("Property '%s' is not present in '%s'!" % (prop, userType.__name__)))
+        self.assertTrue(
+            hasattr(userType, prop),
+            msg=("Property '%s' is not present in '%s'!" % (prop, userType.__name__))
+        )
         propAttr = getattr(userType, prop)
-        self.assertTrue(isinstance(propAttr, property),
-            msg=("Attribute '%s' is not a property in '%s'!" % (prop, userType.__name__)))
-        self.assertIsNotNone(propAttr.fget,
-            msg=("Property '%s' getter is not set in '%s'!" % (prop, userType.__name__)))
+        self.assertTrue(
+            isinstance(propAttr, property),
+            msg=("Attribute '%s' is not a property in '%s'!" % (prop, userType.__name__))
+        )
+        self.assertIsNotNone(
+            propAttr.fget,
+            msg=("Property '%s' getter is not set in '%s'!" % (prop, userType.__name__))
+        )
         if readOnly:
-            self.assertIsNone(propAttr.fset,
-                msg=("Read-only property '%s' setter is set in '%s'!" % (prop, userType.__name__)))
+            self.assertIsNone(
+                propAttr.fset,
+                msg=("Read-only property '%s' setter is set in '%s'!" % (prop, userType.__name__))
+            )
         else:
-            self.assertIsNotNone(propAttr.fset,
-                msg=("Property '%s' setter is not set in '%s'!" % (prop, userType.__name__)))
+            self.assertIsNotNone(
+                propAttr.fset,
+                msg=("Property '%s' setter is not set in '%s'!" % (prop, userType.__name__))
+            )
 
 
 NUM_ELEMENTS = 2
