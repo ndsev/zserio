2fc644b48118c5b875b6738f77cc8f3f993696af
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2021-04-09 10:56:57 +0200

    Change version to 2.3.0

--- compiler/extensions/java/src/zserio/extension/java/JavaExtensionVersion.java
+++ compiler/extensions/java/src/zserio/extension/java/JavaExtensionVersion.java
@@ -9,5 +9,5 @@ package zserio.extension.java;
 public class JavaExtensionVersion
 {
     /** Java extension version string. */
-    public static final String VERSION_STRING = "2.3.0-pre1";
+    public static final String VERSION_STRING = "2.3.0";
 }
