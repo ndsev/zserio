46225b7a7648cf14c8b260babcfc0b292a55547e
Milan Kriz <milan.kriz@eccam.com>
2021-09-01 17:24:02 +0200

    [#309] Prevent clashing of field constructor and packed read constructor

--- compiler/extensions/cpp/runtime/src/zserio/Traits.h
+++ compiler/extensions/cpp/runtime/src/zserio/Traits.h
@@ -2,6 +2,7 @@
 #define ZSERIO_TRAITS_H_INC
 
 #include <type_traits>
+#include "zserio/RebindAlloc.h"
 
 namespace zserio
 {
@@ -9,6 +10,8 @@ namespace zserio
 // forward declaration
 class BitStreamReader;
 struct PropagateAllocatorT;
+template <typename>
+class BasicPackingContextNode;
 
 /**
  * Trait used to enable field constructor only for suitable compound types (using SFINAE).
@@ -16,9 +19,11 @@ struct PropagateAllocatorT;
 template <typename FIELD_TYPE, typename COMPOUND_TYPE, typename ALLOCATOR_TYPE>
 using is_field_constructor_enabled = std::enable_if<
         !std::is_same<typename std::decay<FIELD_TYPE>::type, ALLOCATOR_TYPE>::value &&
-        !std::is_same<typename std::decay<FIELD_TYPE>::type, zserio::BitStreamReader>::value &&
+        !std::is_same<typename std::decay<FIELD_TYPE>::type, BitStreamReader>::value &&
         !std::is_same<typename std::decay<FIELD_TYPE>::type, COMPOUND_TYPE>::value &&
-        !std::is_same<typename std::decay<FIELD_TYPE>::type, zserio::PropagateAllocatorT>::value,
+        !std::is_same<typename std::decay<FIELD_TYPE>::type, PropagateAllocatorT>::value &&
+        !std::is_same<typename std::decay<FIELD_TYPE>::type,
+                BasicPackingContextNode<RebindAlloc<ALLOCATOR_TYPE, uint8_t>>>::value,
         int>;
 
 /**
