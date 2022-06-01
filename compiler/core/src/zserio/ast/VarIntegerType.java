858ae0adaa67afcdfaf07a6e5f8ee11a3aff5677
mrozloznik <mrozloznik@cricetus>
2022-06-01 10:31:55 +0200

    [#322] Remove trailing spaces in VarIntegerType module

--- compiler/core/src/zserio/ast/VarIntegerType.java
+++ compiler/core/src/zserio/ast/VarIntegerType.java
@@ -126,28 +126,31 @@ public class VarIntegerType extends IntegerType
         return maxBitSize;
     }
 
-    public static final BigInteger VARUINT16_UPPER_BOUND = BigInteger.ONE.shiftLeft(15).subtract(BigInteger.ONE); 
+    public static final BigInteger VARUINT16_UPPER_BOUND =
+            BigInteger.ONE.shiftLeft(15).subtract(BigInteger.ONE);
     public static final BigInteger VARUINT16_LOWER_BOUND = BigInteger.ZERO;
 
-    public static final BigInteger VARINT16_UPPER_BOUND = BigInteger.ONE.shiftLeft(14).subtract(BigInteger.ONE); 
+    public static final BigInteger VARINT16_UPPER_BOUND = BigInteger.ONE.shiftLeft(14).subtract(BigInteger.ONE);
     public static final BigInteger VARINT16_LOWER_BOUND = VARINT16_UPPER_BOUND.negate();
 
-    public static final BigInteger VARUINT32_UPPER_BOUND = BigInteger.ONE.shiftLeft(29).subtract(BigInteger.ONE); 
+    public static final BigInteger VARUINT32_UPPER_BOUND =
+            BigInteger.ONE.shiftLeft(29).subtract(BigInteger.ONE);
     public static final BigInteger VARUINT32_LOWER_BOUND = BigInteger.ZERO;
 
-    public static final BigInteger VARINT32_UPPER_BOUND = BigInteger.ONE.shiftLeft(28).subtract(BigInteger.ONE); 
+    public static final BigInteger VARINT32_UPPER_BOUND = BigInteger.ONE.shiftLeft(28).subtract(BigInteger.ONE);
     public static final BigInteger VARINT32_LOWER_BOUND = VARINT32_UPPER_BOUND.negate();
 
-    public static final BigInteger VARUINT64_UPPER_BOUND = BigInteger.ONE.shiftLeft(57).subtract(BigInteger.ONE); 
+    public static final BigInteger VARUINT64_UPPER_BOUND =
+            BigInteger.ONE.shiftLeft(57).subtract(BigInteger.ONE);
     public static final BigInteger VARUINT64_LOWER_BOUND = BigInteger.ZERO;
 
-    public static final BigInteger VARINT64_UPPER_BOUND = BigInteger.ONE.shiftLeft(56).subtract(BigInteger.ONE); 
+    public static final BigInteger VARINT64_UPPER_BOUND = BigInteger.ONE.shiftLeft(56).subtract(BigInteger.ONE);
     public static final BigInteger VARINT64_LOWER_BOUND = VARINT64_UPPER_BOUND.negate();
 
-    public static final BigInteger VARUINT_UPPER_BOUND = BigInteger.ONE.shiftLeft(64).subtract(BigInteger.ONE); 
+    public static final BigInteger VARUINT_UPPER_BOUND = BigInteger.ONE.shiftLeft(64).subtract(BigInteger.ONE);
     public static final BigInteger VARUINT_LOWER_BOUND = BigInteger.ZERO;
 
-    public static final BigInteger VARINT_UPPER_BOUND = BigInteger.valueOf(Long.MAX_VALUE); 
+    public static final BigInteger VARINT_UPPER_BOUND = BigInteger.valueOf(Long.MAX_VALUE);
     public static final BigInteger VARINT_LOWER_BOUND = BigInteger.valueOf(Long.MIN_VALUE);
 
     public static final BigInteger VARSIZE_UPPER_BOUND = BigInteger.ONE.shiftLeft(31).subtract(BigInteger.ONE);
