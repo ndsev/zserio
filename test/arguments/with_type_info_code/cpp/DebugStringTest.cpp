a2239d5c64d72006aaa8d92d412b2f93af5c4694
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2022-07-08 10:57:52 +0200

    [#278] Improve DebugStringTest.cpp

--- test/arguments/with_type_info_code/cpp/DebugStringTest.cpp
+++ test/arguments/with_type_info_code/cpp/DebugStringTest.cpp
@@ -6,6 +6,7 @@
 #include "zserio/SerializeUtil.h"
 #include "zserio/JsonWriter.h"
 #include "zserio/Walker.h"
+#include "zserio/DebugStringUtil.h"
 
 #include "WithTypeInfoCodeCreator.h"
 
@@ -15,6 +16,7 @@ namespace with_type_info_code
 {
 
 using allocator_type = WithTypeInfoCode::allocator_type;
+using string_type = zserio::string<allocator_type>;
 template <typename T>
 using vector_type = zserio::vector<T, allocator_type>;
 
@@ -27,81 +29,74 @@ using ArrayLengthWalkFilter = zserio::BasicArrayLengthWalkFilter<allocator_type>
 using DepthWalkFilter = zserio::BasicDepthWalkFilter<allocator_type>;
 using RegexWalkFilter = zserio::BasicRegexWalkFilter<allocator_type>;
 using AndWalkFilter = zserio::BasicAndWalkFilter<allocator_type>;
+using IReflectablePtr = zserio::IBasicReflectablePtr<allocator_type>;
 
 class DebugStringTest : public ::testing::Test
 {
 protected:
-    std::string getJsonNameWithArrayLengthFilter(size_t arrayLength)
+    string_type getJsonNameWithArrayLengthFilter(size_t arrayLength)
     {
-        return std::string("arguments/with_type_info_code/with_type_info_code_array_length_") +
+        return string_type("arguments/with_type_info_code/with_type_info_code_array_length_") +
                 std::to_string(arrayLength) + ".json";
     }
 
-    void checkJsonFile(const std::string& createdJsonFileName)
+    void checkJsonFile(const string_type& createdJsonFileName)
     {
-        const std::string& createdJsonBaseName = createdJsonFileName.substr(
+        const string_type& createdJsonBaseName = createdJsonFileName.substr(
                 createdJsonFileName.find_last_of("/") + 1);
-        const std::string jsonDataFileName("arguments/with_type_info_code/data/" + createdJsonBaseName);
+        const string_type jsonDataFileName("arguments/with_type_info_code/data/" + createdJsonBaseName);
 
         std::ifstream jsonCreatedFile(createdJsonFileName);
         std::ifstream jsonExpectedFile(jsonDataFileName);
-        std::string createdLine;
-        std::string expectedLine;
+        string_type createdLine;
+        string_type expectedLine;
         while (std::getline(jsonCreatedFile, createdLine) && std::getline(jsonExpectedFile, expectedLine))
             ASSERT_EQ(expectedLine, createdLine);
         ASSERT_TRUE(jsonCreatedFile.eof());
         ASSERT_TRUE(jsonExpectedFile.eof());
     }
 
-    static const std::string JSON_NAME_WRITE_READ_WITH_OPTIONALS;
-    static const std::string JSON_NAME_WRITE_READ_WITHOUT_OPTIONALS;
-    static const std::string JSON_NAME_WITH_OPTIONALS;
-    static const std::string JSON_NAME_WITHOUT_OPTIONALS;
-    static const std::string JSON_NAME_WITH_DEPTH0_FILTER;
-    static const std::string JSON_NAME_WITH_DEPTH5_FILTER;
-    static const std::string JSON_NAME_WITH_DEPTH1_ARRAY_LENGTH0_FILTER;
-    static const std::string JSON_NAME_WITH_REGEX_FILTER;
+    static const string_type JSON_NAME_WRITE_READ_WITH_OPTIONALS;
+    static const string_type JSON_NAME_WRITE_READ_WITHOUT_OPTIONALS;
+    static const string_type JSON_NAME_WITH_OPTIONALS;
+    static const string_type JSON_NAME_WITHOUT_OPTIONALS;
+    static const string_type JSON_NAME_WITH_DEPTH0_FILTER;
+    static const string_type JSON_NAME_WITH_DEPTH5_FILTER;
+    static const string_type JSON_NAME_WITH_DEPTH1_ARRAY_LENGTH0_FILTER;
+    static const string_type JSON_NAME_WITH_REGEX_FILTER;
     static constexpr uint8_t JSON_INDENT = 4;
 
     zserio::BitBuffer bitBuffer = zserio::BitBuffer(1024 * 8);
 };
 
-const std::string DebugStringTest::JSON_NAME_WITH_OPTIONALS =
+const string_type DebugStringTest::JSON_NAME_WITH_OPTIONALS =
         "arguments/with_type_info_code/with_type_info_code_optionals.json";
-const std::string DebugStringTest::JSON_NAME_WITHOUT_OPTIONALS =
+const string_type DebugStringTest::JSON_NAME_WITHOUT_OPTIONALS =
         "arguments/with_type_info_code/with_type_info_code.json";
-const std::string DebugStringTest::JSON_NAME_WITH_DEPTH0_FILTER =
+const string_type DebugStringTest::JSON_NAME_WITH_DEPTH0_FILTER =
         "arguments/with_type_info_code/with_type_info_code_depth0.json";
-const std::string DebugStringTest::JSON_NAME_WITH_DEPTH5_FILTER =
+const string_type DebugStringTest::JSON_NAME_WITH_DEPTH5_FILTER =
         "arguments/with_type_info_code/with_type_info_code_depth5.json";
-const std::string DebugStringTest::JSON_NAME_WITH_DEPTH1_ARRAY_LENGTH0_FILTER =
+const string_type DebugStringTest::JSON_NAME_WITH_DEPTH1_ARRAY_LENGTH0_FILTER =
         "arguments/with_type_info_code/with_type_info_code_depth1_array_length0.json";
-const std::string DebugStringTest::JSON_NAME_WITH_REGEX_FILTER =
+const string_type DebugStringTest::JSON_NAME_WITH_REGEX_FILTER =
         "arguments/with_type_info_code/with_type_info_code_regex.json";
 
 TEST_F(DebugStringTest, jsonWriterWithOptionals)
 {
+    // non-constant reflectable
     WithTypeInfoCode withTypeInfoCode;
     const bool createOptionals = true;
     fillWithTypeInfoCode(withTypeInfoCode, createOptionals);
     withTypeInfoCode.initializeOffsets(0);
 
-    {
-        std::ofstream jsonFile(JSON_NAME_WITH_OPTIONALS, std::ios::out | std::ios::trunc);
-        JsonWriter jsonWriter(jsonFile, JSON_INDENT);
-        Walker walker(jsonWriter);
-        walker.walk(withTypeInfoCode.reflectable());
-        checkJsonFile(JSON_NAME_WITH_OPTIONALS);
-    }
-    {
-        // constant reflectable
-        const WithTypeInfoCode& withTypeInfoCodeConst = withTypeInfoCode;
-        std::ofstream jsonFile(JSON_NAME_WITH_OPTIONALS, std::ios::out | std::ios::trunc);
-        JsonWriter jsonWriter(jsonFile, JSON_INDENT);
-        Walker walker(jsonWriter);
-        walker.walk(withTypeInfoCodeConst.reflectable());
-        checkJsonFile(JSON_NAME_WITH_OPTIONALS);
-    }
+    zserio::toJsonFile(withTypeInfoCode, JSON_NAME_WITH_OPTIONALS);
+    checkJsonFile(JSON_NAME_WITH_OPTIONALS);
+
+    // constant reflectable
+    const WithTypeInfoCode& withTypeInfoCodeConst = withTypeInfoCode;
+    zserio::toJsonFile(withTypeInfoCodeConst, JSON_NAME_WITH_OPTIONALS);
+    checkJsonFile(JSON_NAME_WITH_OPTIONALS);
 }
 
 TEST_F(DebugStringTest, jsonWriterWithoutOptionals)
@@ -137,7 +132,7 @@ TEST_F(DebugStringTest, jsonWriterWithArrayLengthFilter)
 
     for (size_t i = 0; i < 11; ++i)
     {
-        const std::string jsonFileName = getJsonNameWithArrayLengthFilter(i);
+        const string_type jsonFileName = getJsonNameWithArrayLengthFilter(i);
 
         {
             std::ofstream jsonFile(jsonFileName, std::ios::out | std::ios::trunc);
