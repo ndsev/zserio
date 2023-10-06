65f6e75c4e55d33c8bbdfa57d692fef9bea1d836
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2023-10-06 14:01:48 +0200

    Add generated v2.12.0 runtime documentation

---
+++ doc/runtime/latest/cpp/classzserio_1_1TemplatableTypeInfoBase.js
@@ -0,0 +1,11 @@
+var classzserio_1_1TemplatableTypeInfoBase =
+[
+    [ "TemplatableTypeInfoBase", "classzserio_1_1TemplatableTypeInfoBase.html#a5056b98b7657a9f828f5079bb07de4b4", null ],
+    [ "~TemplatableTypeInfoBase", "classzserio_1_1TemplatableTypeInfoBase.html#abf9531c3f4188e63aaee0b5d292dc823", null ],
+    [ "TemplatableTypeInfoBase", "classzserio_1_1TemplatableTypeInfoBase.html#a8edd38decca929c13eec12128c8df443", null ],
+    [ "TemplatableTypeInfoBase", "classzserio_1_1TemplatableTypeInfoBase.html#abb5853f325ee7815870d8d0c67d355e6", null ],
+    [ "getTemplateArguments", "classzserio_1_1TemplatableTypeInfoBase.html#a6b70eab4ca4c772ef2ce88bc682618a3", null ],
+    [ "getTemplateName", "classzserio_1_1TemplatableTypeInfoBase.html#adb7dd0991f1de3637979896827b82afc", null ],
+    [ "operator=", "classzserio_1_1TemplatableTypeInfoBase.html#a837c66f6428b86067219a5b04d98c180", null ],
+    [ "operator=", "classzserio_1_1TemplatableTypeInfoBase.html#a1ff03fb4c3eb6bf86cfb85c8fd519fe0", null ]
+];
