b3ee6887db7b6e582c67c850177eed4395e1936e
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2023-10-06 14:01:40 +0200

    Add generated v2.11.0 runtime documentation

---
+++ doc/runtime/2.11.0/cpp/classzserio_1_1EnumReflectableArray.js
@@ -0,0 +1,14 @@
+var classzserio_1_1EnumReflectableArray =
+[
+    [ "EnumReflectableArray", "classzserio_1_1EnumReflectableArray.html#a043faef8501f4925a44462f29a513b0c", null ],
+    [ "append", "classzserio_1_1EnumReflectableArray.html#aa285c7fdff1902ef508996b5a84b4d2f", null ],
+    [ "at", "classzserio_1_1EnumReflectableArray.html#a0cad5d08a96feb36e6bff287dd0a7f2b", null ],
+    [ "at", "classzserio_1_1EnumReflectableArray.html#a0ef1716888b3b08f7b6efeedff52026e", null ],
+    [ "getAnyValue", "classzserio_1_1EnumReflectableArray.html#ad24b3f7167f739caffa23713049218d4", null ],
+    [ "getAnyValue", "classzserio_1_1EnumReflectableArray.html#a001fd4d5829d461f3cae04681ec8e439", null ],
+    [ "operator[]", "classzserio_1_1EnumReflectableArray.html#a3f110068099479fdd5430e42ef64beac", null ],
+    [ "operator[]", "classzserio_1_1EnumReflectableArray.html#a374549cd1c6e05a1cafc923aa8d7a509", null ],
+    [ "resize", "classzserio_1_1EnumReflectableArray.html#a64ddd11c155047263e83114b50cd026b", null ],
+    [ "setAt", "classzserio_1_1EnumReflectableArray.html#a9e38e163121e2c00ff6b5576a4a64896", null ],
+    [ "size", "classzserio_1_1EnumReflectableArray.html#ab36516060e5133e00cc8e3b710b1306e", null ]
+];
---
+++ doc/runtime/2.11.0/cpp/classzserio_1_1PackedArrayTraits.js
@@ -0,0 +1,10 @@
+var classzserio_1_1PackedArrayTraits =
+[
+    [ "ElementType", "classzserio_1_1PackedArrayTraits.html#ae8b2b4671535fe65ca8657fae0fd1238", null ],
+    [ "PackedArrayTraits", "classzserio_1_1PackedArrayTraits.html#a31d3c4708a70b4f7d6e9de15c91f50c7", null ],
+    [ "bitSizeOf", "classzserio_1_1PackedArrayTraits.html#a0b0f1eedee7304e900f76637ccd47336", null ],
+    [ "initContext", "classzserio_1_1PackedArrayTraits.html#adec176fdf7638ed0537197208d69ffc0", null ],
+    [ "initializeOffsets", "classzserio_1_1PackedArrayTraits.html#a2f6c0a4e40cc4a5f7289bd546da06760", null ],
+    [ "read", "classzserio_1_1PackedArrayTraits.html#af94f81663f823c762155353c8f2ffa5f", null ],
+    [ "write", "classzserio_1_1PackedArrayTraits.html#a291ae40d3111dea60dc571d08d2e65aa", null ]
+];
