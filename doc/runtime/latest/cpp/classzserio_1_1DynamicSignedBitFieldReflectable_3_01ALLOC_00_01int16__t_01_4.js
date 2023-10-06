4cc38555c8870a1747af5df00e36b133040a4181
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2023-10-06 14:01:48 +0200

    Add generated v2.12.0 runtime documentation

---
+++ doc/runtime/latest/cpp/classzserio_1_1DynamicSignedBitFieldReflectable_3_01ALLOC_00_01int16__t_01_4.js
@@ -0,0 +1,6 @@
+var classzserio_1_1DynamicSignedBitFieldReflectable_3_01ALLOC_00_01int16__t_01_4 =
+[
+    [ "DynamicSignedBitFieldReflectable", "classzserio_1_1DynamicSignedBitFieldReflectable_3_01ALLOC_00_01int16__t_01_4.html#a95ff16c3b1b8fd486d7161d62659203e", null ],
+    [ "bitSizeOf", "classzserio_1_1DynamicSignedBitFieldReflectable_3_01ALLOC_00_01int16__t_01_4.html#a8213978aae25d7693748ea5a1203bd19", null ],
+    [ "write", "classzserio_1_1DynamicSignedBitFieldReflectable_3_01ALLOC_00_01int16__t_01_4.html#a8fb94f0f6177683fcfb858c29dd2c1cf", null ]
+];
---
+++ doc/runtime/latest/cpp/search/variables_2.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['cache',['cache',['../structzserio_1_1BitStreamReader_1_1ReaderContext.html#a217494e42b05e4d36bef8f8fe040e5fc',1,'zserio::BitStreamReader::ReaderContext']]],
+  ['cachenumbits',['cacheNumBits',['../structzserio_1_1BitStreamReader_1_1ReaderContext.html#ab1575a7681b173cd5f1a387e1c93979c',1,'zserio::BitStreamReader::ReaderContext']]],
+  ['caseexpressions',['caseExpressions',['../structzserio_1_1BasicCaseInfo.html#aba8c29e66ad867f991ee5d8d5f53f226',1,'zserio::BasicCaseInfo']]],
+  ['constraint',['constraint',['../structzserio_1_1BasicFieldInfo.html#a7fa92bdc0fd038f923ea8a3702e66053',1,'zserio::BasicFieldInfo']]],
+  ['cpp_5fextension_5fruntime_5fversion_5fstring',['CPP_EXTENSION_RUNTIME_VERSION_STRING',['../CppRuntimeVersion_8h.html#a571118b58ed418daa6daf667b07793b9',1,'CppRuntimeVersion.h']]]
+];
