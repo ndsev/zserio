09c91fde782e6de48fd7b229dead36b03f1ac4ce
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2023-10-06 14:01:40 +0200

    Add generated v2.11.0 runtime documentation

---
+++ doc/runtime/2.11.0/cpp/Traits_8h.js
@@ -0,0 +1,16 @@
+var Traits_8h =
+[
+    [ "BasicPackingContextNode", "classzserio_1_1BasicPackingContextNode.html", "classzserio_1_1BasicPackingContextNode" ],
+    [ "Span", "classzserio_1_1Span.html", "classzserio_1_1Span" ],
+    [ "is_allocator", "structzserio_1_1is__allocator.html", null ],
+    [ "is_first_allocator", "structzserio_1_1is__first__allocator.html", null ],
+    [ "is_first_allocator< T, ARGS... >", "structzserio_1_1is__first__allocator_3_01T_00_01ARGS_8_8_8_01_4.html", null ],
+    [ "has_initialize", "structzserio_1_1has__initialize.html", null ],
+    [ "has_initialize_children", "structzserio_1_1has__initialize__children.html", null ],
+    [ "has_reflectable", "structzserio_1_1has__reflectable.html", null ],
+    [ "is_bitmask", "structzserio_1_1is__bitmask.html", null ],
+    [ "is_span", "structzserio_1_1is__span.html", null ],
+    [ "is_span< Span< T, Extent > >", "structzserio_1_1is__span_3_01Span_3_01T_00_01Extent_01_4_01_4.html", null ],
+    [ "is_field_constructor_enabled", "Traits_8h.html#a81ee0edbad335b9af502822475fa3669", null ],
+    [ "is_field_constructor_enabled_t", "Traits_8h.html#a7ae86dff238a9f22729ca7a9537c81da", null ]
+];
---
+++ doc/runtime/2.11.0/cpp/search/typedefs_a.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['objectservicedata',['ObjectServiceData',['../namespacezserio.html#ac453b2be8c699bff370d6e3977c454ec',1,'zserio::ObjectServiceData()'],['../namespacezserio_1_1pmr.html#aa44907c5c108fde18ae0af53ac5018b3',1,'zserio::pmr::ObjectServiceData()']]],
+  ['other',['other',['../structzserio_1_1pmr_1_1PolymorphicAllocator_1_1rebind.html#a6dcfcdd0429364bc73ce7771ed7e00a7',1,'zserio::pmr::PolymorphicAllocator::rebind::other()'],['../structzserio_1_1pmr_1_1PropagatingPolymorphicAllocator_1_1rebind.html#ab705b2dd9d36c137e057763d5f115cec',1,'zserio::pmr::PropagatingPolymorphicAllocator::rebind::other()']]]
+];
