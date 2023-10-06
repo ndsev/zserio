ec1cff7da320e2ec05b7b3f44c9ff32df38c0eca
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2023-10-06 14:01:48 +0200

    Add generated v2.12.0 runtime documentation

---
+++ doc/runtime/2.12.0/cpp/search/functions_b.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['next',['next',['../classzserio_1_1BasicJsonTokenizer.html#a3a05db3ede2f10c6c892951ac49cf795',1,'zserio::BasicJsonTokenizer']]],
+  ['nullopttype',['NullOptType',['../structzserio_1_1NullOptType.html#a856e09717502b2c52e822dcee9dc33c5',1,'zserio::NullOptType']]],
+  ['numbits',['numBits',['../namespacezserio_1_1builtin.html#a9d060458cbd4f5778330033a168fb363',1,'zserio::builtin']]]
+];
---
+++ doc/runtime/latest/cpp/structzserio_1_1BasicTableInfo.js
@@ -0,0 +1,5 @@
+var structzserio_1_1BasicTableInfo =
+[
+    [ "schemaName", "structzserio_1_1BasicTableInfo.html#a4cb245ee61fe0b3cf361102c726f4b83", null ],
+    [ "typeInfo", "structzserio_1_1BasicTableInfo.html#ac8f5246915e45bb67ce9da9a89e96a8f", null ]
+];
