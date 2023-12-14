255f08c26c5f06d8c2c9338879d251d4ac8179f7
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2023-12-14 10:37:23 +0100

    Add generated v2.13.0 runtime documentation

---
+++ doc/runtime/2.13.0/cpp/classzserio_1_1pmr_1_1PolymorphicAllocator.js
@@ -0,0 +1,8 @@
+var classzserio_1_1pmr_1_1PolymorphicAllocator =
+[
+    [ "rebind", "structzserio_1_1pmr_1_1PolymorphicAllocator_1_1rebind.html", "structzserio_1_1pmr_1_1PolymorphicAllocator_1_1rebind" ],
+    [ "propagate_on_container_copy_assignment", "classzserio_1_1pmr_1_1PolymorphicAllocator.html#a0b1176f49360080d07c82f13dbc7572b", null ],
+    [ "propagate_on_container_move_assignment", "classzserio_1_1pmr_1_1PolymorphicAllocator.html#a7bf33287e2697d7dfc94d3a7ea70213e", null ],
+    [ "propagate_on_container_swap", "classzserio_1_1pmr_1_1PolymorphicAllocator.html#acccf6da12834532cd8c5bb4accabc51d", null ],
+    [ "select_on_container_copy_construction", "classzserio_1_1pmr_1_1PolymorphicAllocator.html#a1e5a9823b94fd7eab486996fa8fc6263", null ]
+];
---
+++ doc/runtime/2.13.0/cpp/pmr_2AnyHolder_8h.js
@@ -0,0 +1,4 @@
+var pmr_2AnyHolder_8h =
+[
+    [ "AnyHolder", "pmr_2AnyHolder_8h.html#af3cd42e908869ca9fadc2f0c8ea23166", null ]
+];
