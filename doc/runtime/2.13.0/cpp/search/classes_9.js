ef4bf7847ac554290158b783f3a8ec463314c085
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2023-12-14 10:37:23 +0100

    Add generated v2.13.0 runtime documentation

---
+++ doc/runtime/2.13.0/cpp/search/classes_9.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['memoryresource',['MemoryResource',['../classzserio_1_1pmr_1_1MemoryResource.html',1,'zserio::pmr']]]
+];
--- doc/runtime/latest/cpp/classzserio_1_1BitStreamWriter_1_1InsufficientCapacityException.js
+++
@@ -1,6 +0,0 @@
-var classzserio_1_1BitStreamWriter_1_1InsufficientCapacityException =
-[
-    [ "CppRuntimeException", "classzserio_1_1BitStreamWriter_1_1InsufficientCapacityException.html#ac5d450feffd46dff4d4896173bc2c9cd", null ],
-    [ "CppRuntimeException", "classzserio_1_1BitStreamWriter_1_1InsufficientCapacityException.html#a8f55877c603ac6c6fd78e490caf1af62", null ],
-    [ "CppRuntimeException", "classzserio_1_1BitStreamWriter_1_1InsufficientCapacityException.html#a878158ca023407ed6252ffb50c563c78", null ]
-];
