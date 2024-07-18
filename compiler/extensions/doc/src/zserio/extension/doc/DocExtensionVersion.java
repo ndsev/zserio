8d3718f785e710e75f42764b33828a75630baba1
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2024-07-18 11:16:55 +0200

    Update Doc extension version to 1.0.2

--- compiler/extensions/doc/src/zserio/extension/doc/DocExtensionVersion.java
+++ compiler/extensions/doc/src/zserio/extension/doc/DocExtensionVersion.java
@@ -10,7 +10,7 @@ package zserio.extension.doc;
 final class DocExtensionVersion
 {
     /** Documentation extension version string. */
-    public static final String DOC_EXTENSION_VERSION_STRING = "1.0.1";
+    public static final String DOC_EXTENSION_VERSION_STRING = "1.0.2";
 
     /** Expected Zserio core version string. */
     public static final String EXPECTED_ZSERIO_VERSION_STRING = "2.14.1";
