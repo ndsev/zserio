50eeec5b41f15d290e6d4d0234cfe57dc14f55ad
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2024-10-30 10:11:51 +0100

    Add generated v2.15.0 runtime documentation

---
+++ doc/runtime/2.15.0/cpp/SerializeUtil_8h.js
@@ -0,0 +1,15 @@
+var SerializeUtil_8h =
+[
+    [ "deserialize", "SerializeUtil_8h.html#ae191f28f267323fce42e98bc9b24d1dd", null ],
+    [ "deserialize", "SerializeUtil_8h.html#a531e93a20b762671772e09c3b84cb4c5", null ],
+    [ "deserializeFromBytes", "SerializeUtil_8h.html#a47f381fc3119ee1f0467c41eaa529854", null ],
+    [ "deserializeFromBytes", "SerializeUtil_8h.html#a16c15f5125a6cb07a49ce1d42bcb2341", null ],
+    [ "deserializeFromFile", "SerializeUtil_8h.html#acbd183f9b3c0c0a915337c904513f5cd", null ],
+    [ "serialize", "SerializeUtil_8h.html#a636c389aad006d26195a6dd0f13b7aa6", null ],
+    [ "serialize", "SerializeUtil_8h.html#a9e8d20bb577590dce1b74634133646fd", null ],
+    [ "serialize", "SerializeUtil_8h.html#af5eca1472ffb2128a9052a163b175c1c", null ],
+    [ "serializeToBytes", "SerializeUtil_8h.html#ab42bdef12707f9f6324cc752c053bb08", null ],
+    [ "serializeToBytes", "SerializeUtil_8h.html#a4a6337582a49b4ed55134e9d690b915a", null ],
+    [ "serializeToBytes", "SerializeUtil_8h.html#a7467a7bd458b6d393527a691815db303", null ],
+    [ "serializeToFile", "SerializeUtil_8h.html#a886035a088f5a9b7e6243a16c9e0ad11", null ]
+];
---
+++ doc/runtime/2.15.0/cpp/classzserio_1_1UInt16ReflectableBase.js
@@ -0,0 +1,5 @@
+var classzserio_1_1UInt16ReflectableBase =
+[
+    [ "Base", "classzserio_1_1UInt16ReflectableBase.html#a68ed0007f853f580d6ac96f1aa4566f3", null ],
+    [ "getUInt16", "classzserio_1_1UInt16ReflectableBase.html#ac445fc9cd2ece9ba058518442cee75b5", null ]
+];
