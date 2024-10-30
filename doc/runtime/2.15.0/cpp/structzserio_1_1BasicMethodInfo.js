3b32467cb51a412813dc945b03f4cf9a61286746
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2024-10-30 10:11:51 +0100

    Add generated v2.15.0 runtime documentation

---
+++ doc/runtime/2.15.0/cpp/structzserio_1_1BasicMethodInfo.js
@@ -0,0 +1,6 @@
+var structzserio_1_1BasicMethodInfo =
+[
+    [ "requestTypeInfo", "structzserio_1_1BasicMethodInfo.html#ab472198d90fca7535b11e270ae2cc020", null ],
+    [ "responseTypeInfo", "structzserio_1_1BasicMethodInfo.html#ae46c06ba442408c828903596ad497b87", null ],
+    [ "schemaName", "structzserio_1_1BasicMethodInfo.html#abcc6360f3107d95977954baa59571423", null ]
+];
