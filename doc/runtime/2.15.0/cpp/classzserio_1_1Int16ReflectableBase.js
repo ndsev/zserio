aedce947e61e891146007222dc17896bf94623a7
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2024-10-30 10:11:51 +0100

    Add generated v2.15.0 runtime documentation

---
+++ doc/runtime/2.15.0/cpp/classzserio_1_1Int16ReflectableBase.js
@@ -0,0 +1,5 @@
+var classzserio_1_1Int16ReflectableBase =
+[
+    [ "Base", "classzserio_1_1Int16ReflectableBase.html#a5f7746ef61e656d6a4ec8b720421b28e", null ],
+    [ "getInt16", "classzserio_1_1Int16ReflectableBase.html#aa4b4227dc2f61bc1ea1837feb756ae8e", null ]
+];
