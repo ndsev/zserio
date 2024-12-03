a4659cc8f422faec79cdad25702682f45ac15658
Milan Kriz <milan.kriz@eccam.com>
2024-12-03 17:49:20 +0100

    Try to fix mingw sign-conversion warning

--- compiler/extensions/cpp/src/zserio/extension/cpp/CppExpressionFormattingPolicy.java
+++ compiler/extensions/cpp/src/zserio/extension/cpp/CppExpressionFormattingPolicy.java
@@ -252,7 +252,7 @@ public class CppExpressionFormattingPolicy extends DefaultExpressionFormattingPo
         }
         else
         {
-            return new BinaryExpressionFormatting("(", ") * (1 << (", "))");
+            return new BinaryExpressionFormatting("(", ") * (1U << (", "))");
         }
     }
 
