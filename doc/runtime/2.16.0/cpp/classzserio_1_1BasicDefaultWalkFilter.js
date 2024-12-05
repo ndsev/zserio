7e2330eb9a6d98cca440989a703e663d5e32ac0b
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2024-12-05 15:01:11 +0100

    Add generated v2.16.0 runtime documentation

---
+++ doc/runtime/2.16.0/cpp/classzserio_1_1BasicDefaultWalkFilter.js
@@ -0,0 +1,15 @@
+var classzserio_1_1BasicDefaultWalkFilter =
+[
+    [ "BasicDefaultWalkFilter", "classzserio_1_1BasicDefaultWalkFilter.html#a8eb2c98caffacf2e0015d18837a0c5af", null ],
+    [ "~BasicDefaultWalkFilter", "classzserio_1_1BasicDefaultWalkFilter.html#ac652196f603288ad0b40ed0e62bd7fd6", null ],
+    [ "BasicDefaultWalkFilter", "classzserio_1_1BasicDefaultWalkFilter.html#ab1e05ec3023c50a2dddedf0440f17251", null ],
+    [ "BasicDefaultWalkFilter", "classzserio_1_1BasicDefaultWalkFilter.html#a052cb0fc865bb14eac0f7c29d8cfa3df", null ],
+    [ "afterArray", "classzserio_1_1BasicDefaultWalkFilter.html#ad3435a946c415a5d7e46fd0c7d98cebf", null ],
+    [ "afterCompound", "classzserio_1_1BasicDefaultWalkFilter.html#a842d195cc2d045b7768d204027365759", null ],
+    [ "afterValue", "classzserio_1_1BasicDefaultWalkFilter.html#a69a2e09e183f959b24257fc8b0962e5f", null ],
+    [ "beforeArray", "classzserio_1_1BasicDefaultWalkFilter.html#af988a8190efcedc7e4a5426c1ced198b", null ],
+    [ "beforeCompound", "classzserio_1_1BasicDefaultWalkFilter.html#a2a976349d85ae364978c0d959c729b1e", null ],
+    [ "beforeValue", "classzserio_1_1BasicDefaultWalkFilter.html#a0fb694502f43006ea4bd2a05a35c9b36", null ],
+    [ "operator=", "classzserio_1_1BasicDefaultWalkFilter.html#aaff4e1d577a52bfcfd7258c0e30a7d63", null ],
+    [ "operator=", "classzserio_1_1BasicDefaultWalkFilter.html#a07398cbc3b163467f44cd712c8c38e0f", null ]
+];
---
+++ doc/runtime/2.16.0/cpp/classzserio_1_1ServiceTypeInfo.js
@@ -0,0 +1,5 @@
+var classzserio_1_1ServiceTypeInfo =
+[
+    [ "ServiceTypeInfo", "classzserio_1_1ServiceTypeInfo.html#a869f4b3006bc259c0a6fd1b3c5f1fc77", null ],
+    [ "getMethods", "classzserio_1_1ServiceTypeInfo.html#a7e63f964ddd78296bfdcca3a8bbffa50", null ]
+];
