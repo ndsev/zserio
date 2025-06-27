f5da2064380da7f057b9ee4d143a2805f9896b65
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2025-06-27 13:14:54 +0200

    Add generated v2.16.1 runtime documentation

---
+++ doc/runtime/2.16.1/cpp/classzserio_1_1IBasicWalkFilter.js
@@ -0,0 +1,10 @@
+var classzserio_1_1IBasicWalkFilter =
+[
+    [ "~IBasicWalkFilter", "classzserio_1_1IBasicWalkFilter.html#a52596f5a3928d29443ba5d1bcee3559e", null ],
+    [ "afterArray", "classzserio_1_1IBasicWalkFilter.html#a918ce30327dee6607ad2b6906a696f09", null ],
+    [ "afterCompound", "classzserio_1_1IBasicWalkFilter.html#a59e7fc58f83eeb6cfeb3562c37c1269d", null ],
+    [ "afterValue", "classzserio_1_1IBasicWalkFilter.html#a53b036642a229f13857634a9114fe16a", null ],
+    [ "beforeArray", "classzserio_1_1IBasicWalkFilter.html#a314d1a02c95753a12978be0a36ec5741", null ],
+    [ "beforeCompound", "classzserio_1_1IBasicWalkFilter.html#a2a11e10d8dd8c6e1a5293135b9205b98", null ],
+    [ "beforeValue", "classzserio_1_1IBasicWalkFilter.html#a3edadbbf455ff912f19c12a9410ff80e", null ]
+];
---
+++ doc/runtime/2.16.1/cpp/search/all_16.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['builtin_973',['builtin',['../namespacezserio_1_1builtin.html',1,'zserio']]],
+  ['literals_974',['literals',['../namespacezserio_1_1literals.html',1,'zserio']]],
+  ['pmr_975',['pmr',['../namespacezserio_1_1pmr.html',1,'zserio']]],
+  ['zserio_976',['zserio',['../namespacezserio.html',1,'']]],
+  ['zserio_20c_2b_2b_20runtime_20library_20documentation_977',['Zserio C++ runtime library documentation',['../index.html',1,'']]],
+  ['zserio_5fdeprecated_978',['ZSERIO_DEPRECATED',['../DeprecatedAttribute_8h.html#a707fe3c0b0a70af7a2d8902fad0ac665',1,'DeprecatedAttribute.h']]],
+  ['zserio_5fruntime_5f64bit_979',['ZSERIO_RUNTIME_64BIT',['../RuntimeArch_8h.html#a49d89387b93cec61765e9453c43059b5',1,'RuntimeArch.h']]],
+  ['zseriotreecreator_980',['ZserioTreeCreator',['../namespacezserio.html#ac235e8cc15ece1065fccfc737316f269',1,'zserio']]],
+  ['zseriotreecreator_2ecpp_981',['ZserioTreeCreator.cpp',['../ZserioTreeCreator_8cpp.html',1,'']]],
+  ['zseriotreecreator_2eh_982',['ZserioTreeCreator.h',['../ZserioTreeCreator_8h.html',1,'']]]
+];
