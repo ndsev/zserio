9dee4b672ce603d9a6cfb4f781ea1dd5730e44e0
Tomas Pecholt <tomas.pecholt@eccam.com>
2025-09-17 18:21:23 +0530

    [#566] Make SqlTable read/write/update more efficient (python)

--- test/extensions/language/sql_tables/python/BlobParamTableTest.py
+++ test/extensions/language/sql_tables/python/BlobParamTableTest.py
@@ -23,6 +23,23 @@ class BlobParamTableTest(SqlTables.TestCaseWithDb):
         testTable.create_table()
         self.assertTrue(self._isTableInDb())
 
+    def testWriteColumns(self):
+        testTable = self._database.blob_param_table
+
+        writtenRows = []
+        writtenRows.append(self._createBlobParamTableRow(999, "Pepek"))
+        writtenRows.append(self._createBlobParamTableRow(999, "Rishi"))
+
+        columns = ["name"]
+        testTable.write(writtenRows, columns)
+
+        readRows = testTable.read()
+        for i, readRow in enumerate(readRows):
+            self.assertEqual(i + 1, readRow[testTable.Column.BLOB_ID])
+            self.assertEqual(writtenRows[i][testTable.Column.NAME], readRow[testTable.Column.NAME])
+            self.assertTrue(readRow[testTable.Column.BLOB] is None)
+            self.assertTrue(readRow[testTable.Column.PARAMETERS] is None)
+
     def testReadWithoutCondition(self):
         testTable = self._database.blob_param_table
 
@@ -49,6 +66,24 @@ class BlobParamTableTest(SqlTables.TestCaseWithDb):
         for readRow in readRows:
             self.assertEqual(writtenRows[expectedRowNum], readRow)
 
+    def testReadColumns(self):
+        testTable = self._database.blob_param_table
+
+        writtenRows = self._createBlobParamTableRows()
+        testTable.write(writtenRows)
+
+        condition = "name='Name1'"
+        columns = ["blobId", "name"]
+        readRows = testTable.read_columns(columns, condition)
+        expectedRowNum = 1
+        for readRow in readRows:
+            self.assertEqual(
+                writtenRows[expectedRowNum][testTable.Column.BLOB_ID], readRow[testTable.Column.BLOB_ID]
+            )
+            self.assertEqual(writtenRows[expectedRowNum][testTable.Column.NAME], readRow[testTable.Column.NAME])
+            self.assertTrue(readRow[testTable.Column.BLOB] is None)
+            self.assertTrue(readRow[testTable.Column.PARAMETERS] is None)
+
     def testUpdate(self):
         testTable = self._database.blob_param_table
 
@@ -63,6 +98,28 @@ class BlobParamTableTest(SqlTables.TestCaseWithDb):
         readRows = testTable.read(updateCondition)
         for readRow in readRows:
             self.assertEqual(updateRow, readRow)
+
+    def testUpdateColumns(self):
+        testTable = self._database.blob_param_table
+
+        writtenRows = self._createBlobParamTableRows()
+        testTable.write(writtenRows)
+
+        updateRowId = 3
+        columns = ["name"]
+        updateRow = self._createBlobParamTableRow(-999, "Pepek")
+        updateCondition = "blobId=" + str(updateRowId)
+        testTable.update_columns(updateRow, columns, updateCondition)
+
+        writtenRow = writtenRows[updateRowId]
+        self.assertEqual(writtenRow[testTable.Column.BLOB_ID], updateRowId)
+        readRows = testTable.read(updateCondition)
+        for readRow in readRows:
+            for i, readColumn in enumerate(readRow):
+                if i == testTable.Column.NAME:
+                    self.assertEqual(updateRow[i], readColumn)
+                else:
+                    self.assertEqual(writtenRow[i], readColumn)
 
     def testNullValues(self):
         testTable = self._database.blob_param_table
