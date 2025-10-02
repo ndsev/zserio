616965e4c259d2df87f87fd42e202b3c0e84b141
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2025-10-02 16:08:51 +0200

    Update Java extension version to 1.2.0

--- compiler/extensions/java/src/zserio/extension/java/JavaExtensionVersion.java
+++ compiler/extensions/java/src/zserio/extension/java/JavaExtensionVersion.java
@@ -10,7 +10,7 @@ package zserio.extension.java;
 public final class JavaExtensionVersion
 {
     /** Java extension version string. */
-    public static final String JAVA_EXTENSION_VERSION_STRING = "1.1.5";
+    public static final String JAVA_EXTENSION_VERSION_STRING = "1.2.0";
 
     /** Expected Zserio core version string. */
     public static final String EXPECTED_ZSERIO_VERSION_STRING = "2.18.0";
