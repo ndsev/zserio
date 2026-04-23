f0767b035a66ec9edcdcbcec9f1a761ea1c05ddc
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2026-04-23 17:47:25 +0200

    Add generated v2.18.1 runtime documentation

---
+++ doc/runtime/2.18.1/cpp/classzserio_1_1UInt32ReflectableBase.js
@@ -0,0 +1,5 @@
+var classzserio_1_1UInt32ReflectableBase =
+[
+    [ "Base", "classzserio_1_1UInt32ReflectableBase.html#aa2ce828ff66a411305a199f7b5ba0ea8", null ],
+    [ "getUInt32", "classzserio_1_1UInt32ReflectableBase.html#af6333c8a931fb49f8ba02894a04c304b", null ]
+];
---
+++ doc/runtime/2.18.1/cpp/pmr_2Vector_8h.js
@@ -0,0 +1,4 @@
+var pmr_2Vector_8h =
+[
+    [ "vector", "pmr_2Vector_8h.html#af1ed529e4f188f8188713fc5794e6ff9", null ]
+];
