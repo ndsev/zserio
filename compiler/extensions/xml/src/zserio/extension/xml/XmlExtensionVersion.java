23223a22a160a94ba2f1affce5dd1fa4fc0c5cb7
Miki Rozloznik <mikulas.rozloznik@eccam.com>
2026-09-17 08:24:44 +0200

    Update Xml extension version to 1.0.10

--- compiler/extensions/xml/src/zserio/extension/xml/XmlExtensionVersion.java
+++ compiler/extensions/xml/src/zserio/extension/xml/XmlExtensionVersion.java
@@ -10,7 +10,7 @@ package zserio.extension.xml;
 public final class XmlExtensionVersion
 {
     /** XML extension version string. */
-    public static final String XML_EXTENSION_VERSION_STRING = "1.0.9";
+    public static final String XML_EXTENSION_VERSION_STRING = "1.0.10";
 
     /** Expected Zserio core version string. */
     public static final String EXPECTED_ZSERIO_VERSION_STRING = "2.19.0";
